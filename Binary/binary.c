880236cc34c8a53af9107b97dba60c44ace655b9
Aromal.sp <150415644+Aromalsp123@users.noreply.github.com>
2024-11-02 21:09:47 +0530

    binary.c

--- Binary/binary.c
+++ Binary/binary.c
@@ -1,33 +1,44 @@
-#include<stdio.h>
-int main() 
-{
-int i,n,element,key,a[100],flag=0;
-printf("Enter the size of the array: ");
-scanf("%d",&n);
-printf("Enter the element on the array:\n");  
 
-for( i=0;i<n;i++)
-{  
-   scanf("%d",&a[i]); // elements inputing...
- }
-printf("Enter the search key :");
-scanf("%d",&key);
-  // Elements searching...
-    for(i=0;i<n;i++)
-      {
+#include <stdio.h>
+
+int main() {
+    int first, mid, last, size, key, i;
+    int a[10];  // Array with a fixed size of 10
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    printf("Enter the elements of the array in sorted order:\n");
+    for (i = 0; i < size; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter the key to find: ");
+    scanf("%d", &key);
+
+    // Initialize first and last for binary search
+    first = 0;
+    last = size - 1;
     
-        if(a[i]==key){
-            flag = 1; // If flag is true "if" statment will print otherwise "else"statment
+    int found = 0;  // Flag to check if the element is found
+
+    while (first <= last) {
+        mid = (first + last) / 2;
+
+        if (a[mid] == key) {
+            printf("Element found at index %d\n", mid);
+            found = 1;  // Mark as found
             break;
+        } else if (a[mid] > key) {
+            last = mid - 1;
+        } else {
+            first = mid + 1;
         }
-      }
-if(flag == 1)
-  { 
-   printf("The key %d is found at postion %d",key,i+1);
-  }
-   else {
-   printf("The key not found %d ",key);
-  }
-  
-return 0;
+    }
+
+    if (!found) {
+        printf("Element not found in the array.\n");
+    }
+
+    return 0;
 }
